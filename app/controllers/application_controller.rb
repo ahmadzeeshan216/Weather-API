acedc5a48dc76607c1594504a83d793eade89028
Ahmad Zeeshan <ahmadzeeshan216@gmail.com>
2022-01-29 15:18:31 -0800

    Moved respective details to application controller

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,6 +1,16 @@
 class ApplicationController < ActionController::API
+  before_action :format_params
+  before_action :set_language
 
   protected
+
+  def format_params
+    params[:locale] = 'en' unless params[:locale].present?
+  end
+
+  def set_language
+    I18n.locale = params[:locale].to_s
+  end
 
   def symbolized_action_name
     action_name.to_sym
--- app/controllers/weather_controller.rb
+++ app/controllers/weather_controller.rb
@@ -1,9 +1,7 @@
 class WeatherController < ApplicationController
   include Weather::Constants
 
-  before_action :format_params
   before_action :validate_params
-  before_action :set_language
   before_action :set_weather
 
   def temperature
@@ -20,23 +18,15 @@ class WeatherController < ApplicationController
 
 private
 
-  def set_weather
-    @weather = RegionalWeather.new(symbolized_action_name => unit_param_value)
-  end
-
   def validate_params
     render status: :unprocessable_entity unless unit_param_value.present?
   end
 
+  def set_weather
+    @weather = RegionalWeather.new(symbolized_action_name => unit_param_value)
+  end
+
   def unit_param_value
     @unit_param ||= params[UNIT_PARAM_NAMES[symbolized_action_name]]
-  end
-
-  def format_params
-    params[:locale] = 'en' unless params[:locale].present?
-  end
-
-  def set_language
-    I18n.locale = params[:locale].to_s
   end
 end
